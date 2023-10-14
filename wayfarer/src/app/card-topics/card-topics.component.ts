c15be24a8f9a10e5f0ef978acebfda7883f7a376
Shayla <shayladwhite1@gmail.com>
2023-10-14 10:09:53 -0700

    Created Card Topics

---
+++ wayfarer/src/app/card-topics/card-topics.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-topics',
+  templateUrl: './card-topics.component.html',
+  styleUrls: ['./card-topics.component.css']
+})
+export class CardTopicsComponent {
+
+}
